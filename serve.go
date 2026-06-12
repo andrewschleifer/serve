6abe94d709d13f2753304ddc0bbf1547adebc3f6
Andrew Schleifer <me@andrewschleifer.name>
2026-06-11 22:38:58 -0400

    make port and directory configurable

--- serve.go
+++ serve.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+  "flag"
+  "fmt"
   "log"
   "net/http"
+  "os"
+)
+
+const (
+  host = "127.0.0.1"
+  port = "8008"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("."))
+  p := flag.String("p", port, "port to listen on")
+  flag.Usage = func() {
+    fmt.Fprintf(os.Stderr, "usage: %s [-p port] <directory>\n", os.Args[0])
+  }
+  flag.Parse()
+
+  if flag.NArg() != 1 {
+    flag.Usage()
+    os.Exit(1)
+  }
+
+  dir := flag.Arg(0)
+  fs := http.FileServer(http.Dir(dir))
   http.Handle("/", fs)
 
-  log.Println("Listening...")
-  log.Fatal(http.ListenAndServe("127.0.0.1:8008", nil))
+  addr := host + ":" + *p
+  log.Println("Listening on http://" + addr)
+  log.Fatal(http.ListenAndServe(addr, nil))
 }
